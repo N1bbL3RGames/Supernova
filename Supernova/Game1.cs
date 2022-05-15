f160bcb38910bb00d67270bfd0fe4178a1ed7018
ColeElfstrom <cole.elfstrom.368@k12.friscoisd.org>
2022-05-15 16:59:40 -0500

    Initial commit

---
+++ Supernova/Game1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Supernova
+{
+    internal class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public Game1() : base()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+
+            this.IsFixedTimeStep = true;
+            this.IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.spriteBatch.Begin();
+
+            this.spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
